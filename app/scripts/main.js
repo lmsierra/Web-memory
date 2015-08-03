41062ea2966f3a75238b7cdf374054715fe7a35c
Mandaca <mandaca99@gmail.com>
2015-08-03 08:34:17 +0200

    primer avance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,18 @@ $("#logo").on('click','button',function () {
 	$("#logo").hide();
 	$("#header").css("display","inherit");
 	$("#nivel").css("display","inherit");
+	$("#cuerpo").show();
 	return false;
 });
 
-$("header").on('click','a',function () {
+$("#header").on('click','a',function () {
 	$("#logo").show();
 	$("#header").hide();
 	$("#nivel").hide();
+	return false;
+});
+
+$("#cuerpo").on('click','button',function () {
+	$("#cuerpo").hide();
 	return false;
 });

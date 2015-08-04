b4b8b0c2e83b877a739ffc257f3a58c67c9b3475
lmsierra <lmsierrod@gmail.com>
2015-08-04 14:39:40 +0200

    asdfg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _tamanio;
-
 document.getElementById('empezarBtn').addEventListener('click', function(){
 	document.getElementById('logo').style.display = 'none';
     document.getElementById('header').style.display = 'block';
@@ -16,7 +14,9 @@ for(var i = 0; i < _opcionesDificultad.length; i++){
 	_opcionesDificultad[i].addEventListener('click', function(){
 	    
 	    document.getElementById('cuerpo').style.display = 'none';
-	    _tamanio = this.textContent.split('X')[0];
+	    var _dificultad = this.textContent.split('X')[0];
+
+	    iniciarJuego(_dificultad);
 
 		return false;
 	});
@@ -28,3 +28,52 @@ document.getElementById('home').addEventListener('click', function(){
     document.getElementById('nivel').style.display = 'none';
     return false;
 });
+
+
+function iniciarJuego(dificultad){
+
+
+	var html1 = '<div id="primeraFila" class="row fila"></div><div id="segundaFila"class="row fila"></div>';
+	document.getElementById('juego').innerHTML = html1;
+
+
+
+
+	for(var i = 0; i < dificultad; i++){
+
+		var div = document.createElement('DIV');
+		div.className = 'row foto';
+		document.getElementById('juego').appendChild(div);
+		
+		for(var j = 0; j < dificultad; j++){
+
+			var _row = document.getElementById('juego').getElementsByClassName('row')[i];
+			_row.insertAdjacentHTML('beforeend','<div class="foto"><a href="#"><img src="/images/rana.jpg"></a></div>');
+		}
+	}
+
+	var _fotos = document.getElementById('juego').getElementsByTagName('img');
+	for(var i = 0; i < _fotos.length; i++){
+		/*
+		var x = 100/dificultad;
+
+		console.log(_fotos[i].offsetWidth);
+		var w = _fotos[i].offsetWidth * x;
+		var h = _fotos[i].offsetHeight * x;
+
+		//var tamanio = (w < h)? tamanio = h : tamanio = w;
+		console.log(w); 
+		console.log(h);
+
+		//_fotos[i].style.width = tamanio;
+		*/
+
+		var y = window.innerHeight;
+		var z = document.getElementById('header').offsetHeight;
+		console.log(y);
+		console.log(z);
+
+		_fotos[i].style.width = ((window.innerWidth - 300)/dificultad) + 'px';
+		_fotos[i].style.height = ((window.innerHeight - document.getElementById('header').offsetHeight -30)/dificultad) + 'px';
+	}
+}

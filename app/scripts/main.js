ac6d3bdb04bef5e3dbf39cd205617f59c78a41c3
lmsierra <lmsierrod@gmail.com>
2015-08-04 11:41:33 +0200

    Paso a JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,29 @@
-// jshint devel:true
-$("#logo").on('click','button',function () {
-	$("#logo").hide();
-	$("#header").css("display","inherit");
-	$("#nivel").css("display","inherit");
-	$("#cuerpo").show();
-	return false;
+'use strict';
+
+(function init() {
+
+    var _logoButtons = [];
+    _logoButtons = document.getElementById('logo').getElementsByTagName('button');
+
+    for (var i = 0; i < _logoButtons.length; i++) {
+        _logoButtons[i].addEventListener('click', function() {
+            $("#logo").hide();
+            $("#header").show();
+            $("#nivel").show();
+        });
+    }
+}());
+
+$("#header").on('click', 'a', function() {
+    $("#logo").show();
+    $("#header").hide();
+    $("#nivel").hide();
+    return false;
 });
 
-$("#header").on('click','a',function () {
-	$("#logo").show();
-	$("#header").hide();
-	$("#nivel").hide();
-	return false;
-});
-
-$("#cuerpo").on('click','button',function () {
-	$("#cuerpo").hide();
-	alert($(this).text());
-	console.log($("button"));
-	return false;
+$("#cuerpo").on('click', 'button', function() {
+    $("#cuerpo").hide();
+    alert($(this).text());
+    console.log($("button"));
+    return false;
 });

83b6774ca082b0155e1b881f8e088736a7ea2952
lmsierra <lmsierrod@gmail.com>
2015-08-05 13:17:54 +0200

    Añadidas imagenes de fondo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var imagenes = ['aguila', 'buho' , 'camaleon', 'cangrejo', 'castor', 'cocodrilo', 'delfin', 'elefante',
+ 'escarabajo', 'escorpion', 'hipopotamo', 'jabali', 'jirafa', 'martin', 'mono', 'morsa', 'oso',
+ 'panda','pandarojo','pez','pinguinos','polar','rana','raya','rinoceronte','salamandra','sepia',
+ 'serpiente','tiburon','tigre','tortuga','tucan'];
+
 document.getElementById('empezarBtn').addEventListener('click', function(){
 	document.getElementById('logo').style.display = 'none';
     document.getElementById('header').style.display = 'block';
@@ -31,6 +36,11 @@ document.getElementById('home').addEventListener('click', function(){
 
 function iniciarJuego(dificultad){
 
+	var _apariciones = new Array(imagenes.length);
+
+	for(var i = 0; i < _apariciones.length; i++){
+		_apariciones[i] = 0;
+	}
 
 	document.getElementById('juego').innerHTML = "";
 
@@ -43,7 +53,9 @@ function iniciarJuego(dificultad){
 
 			var _row = document.getElementById('juego').getElementsByClassName('row')[i];
 
-			_row.innerHTML = _row.innerHTML + '<div class="foto"><a href="#"><img src="/images/fondo.png"/></a></div>';
+			_row.innerHTML = _row.innerHTML + '<div class="foto"><a href="#"><img class="imagen" src="/images/' +
+			 imagenes[generarImagen(_apariciones, dificultad)] + 
+			 '.jpg"/><img class="fondo" src="/images/fondo.png"></a></div>';
 		}
 	}
 
@@ -54,4 +66,17 @@ function iniciarJuego(dificultad){
 		_fotos[i].style.width = ((window.innerWidth - 300)/dificultad) + 'px';
 		_fotos[i].style.height = ((window.innerHeight - document.getElementById('header').offsetHeight -30)/dificultad) + 'px';
 	}
+}
+
+function generarImagen(apariciones, dificultad){
+
+	var _random = Math.floor((Math.random() *1000) % (dificultad * dificultad / 2));
+
+	while(apariciones[_random] > 1){
+		_random = Math.floor((Math.random() *1000) % (dificultad * dificultad / 2));
+	}
+
+	apariciones[_random] ++;
+	console.log(_random);
+	return _random;
 }
